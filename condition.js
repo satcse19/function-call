81a9b3a9a9889d8149768372768f948e60dd61a5
satcse19 <81626145+satcse19@users.noreply.github.com>
2021-10-27 09:06:40 +0600

    cal

---
+++ condition.js
@@ -0,0 +1,21 @@
+function a() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+
+  document.getElementById("result").value = n1 + n2;
+}
+function b() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+  document.getElementById("result").value = n1 - n2;
+}
+function c() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+  document.getElementById("result").value = n1 * n2;
+}
+function d() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+  document.getElementById("result").value = n1 / n2;
+}
